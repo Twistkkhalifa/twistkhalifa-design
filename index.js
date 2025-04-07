7d4b4839c056fc657689d3d3c423cbc6ef986bb5
Twist khalifa <twistakhalifa@gmail.com>
2025-04-07 17:52:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', textAlign: 'center', padding: '2rem' }}>
+      <h1>Bem-vindo ao site do Twist Khalifa</h1>
+      <p>Este site está em construção. Fique ligado para novidades!</p>
+    </div>
+  )
+}
